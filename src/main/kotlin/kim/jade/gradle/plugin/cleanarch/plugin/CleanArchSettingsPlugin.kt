6725fd1bec175e75678a44e0dd99ff7f392da2f7
Jade Kim <me@jade.kim>
2026-07-06 10:59:11 +0900

    add: clean architecture settings plugin

---
+++ src/main/kotlin/kim/jade/gradle/plugin/cleanarch/plugin/CleanArchSettingsPlugin.kt
@@ -0,0 +1,9 @@
+package kim.jade.gradle.plugin.cleanarch.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.initialization.Settings
+
+class CleanArchSettingsPlugin : Plugin<Settings> {
+    override fun apply(target: Settings) {
+    }
+}
